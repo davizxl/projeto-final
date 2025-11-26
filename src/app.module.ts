44538d4e757392560f269ed29dd16c0e69cc3fb9
vitoria-1702 <vih.barbosa1702@gmail.com>
2025-11-25 21:18:39 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoursesModule } from './course/courses.module';
 
 @Module({
   imports: [
@@ -13,13 +14,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'mysql',
-        host: config.get('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASS'),
-        database: config.get('DB_NAME'),
+        host: config.get<string>('DB_HOST') ?? 'localhost',
+        port: config.get<number>('DB_PORT') ?? 3306,
+        username: config.get<string>('DB_USER') ?? 'root',
+        password: config.get<string>('DB_PASS') ?? '',
+        database: config.get<string>('DB_NAME') ?? 'escola_mari',
         autoLoadEntities: true,
-        synchronize: false,
+        synchronize: true,
       }),
     }),
   ],
